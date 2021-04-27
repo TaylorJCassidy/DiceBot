6b4afe7b0ee5d32bc76f1360ed58fcfc5be14d44
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-04-27 19:23:40 +0100

    added prefix reading and writing calls

--- src/repository/Repository.js
+++ src/repository/Repository.js
@@ -1,15 +1,19 @@
+const JSONDAO = require('../DAOs/JSONDAO.js')
+
 class Repository {
     dao;
 
     constructor(guildID) {
-        dao = new JSONDAO(guildID);
+        this.dao = new JSONDAO(guildID);
     }
 
     getPrefix() {
-        
+        return this.dao.getPrefix();
     }
 
     setPrefix() {
-
+        return this.dao.setPrefix();
     }
 }
+
+module.exports = Repository;
