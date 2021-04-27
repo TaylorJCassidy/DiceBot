24688bc953352920aca5c4125b183b272c6bb114
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-04-27 19:22:30 +0100

    added prefix read and write functionality

--- src/DAOs/JSONDAO.js
+++ src/DAOs/JSONDAO.js
@@ -1,16 +1,40 @@
+const Guild = require("../models/Guilds.js")
+const fs = require('fs');
+
 class JSONDAO {
 
-    guildID
+    static filepath = './guildConfigs/';
+    guildID;
+    filename;
 
     constructor(guildID) {
         this.guildID = guildID;
+        this.filename = `${JSONDAO.filepath}${this.guildID}.json`
+    }
+
+    get jsonfile() {
+        try {
+            return JSON.parse(fs.readFileSync(this.filename));
+        }
+        catch (e) {
+            return null;
+        }
     }
 
     getPrefix() {
-
+        let guild = this.jsonfile;
+        if (guild === null) {
+            guild = new Guild();
+            fs.writeFileSync(this.filename,JSON.stringify(guild));
+        }
+        return guild.prefix;
     }
 
-    setPrefix() {
-        
+    setPrefix(prefix) {
+        const guild = this.jsonfile;
+        guild.prefix = prefix;
+        fs.writeFileSync(this.filename,JSON.stringify(guild));
     }
 }
+
+module.exports = JSONDAO;
