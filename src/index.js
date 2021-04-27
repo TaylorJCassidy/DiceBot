ff31fe7cda3ad9a66570a1ca56a9c3f4e5342dae
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-04-27 19:24:46 +0100

    finished initial functionality calls

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ const {token} = require("./config/token.json");
 
 const client = new Discord.Client();
 const commands = require('./utils/getCommands.js').getCommands();
+let guildRepos;
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    guildRepos = require('./utils/getGuildRepos.js').getGuildRepos(client.guilds);
 });
 
 client.on('message', msg => {
-    let args;
-    commands.get(msg.content).run(msg,args);
+    if (guildRepos.get(msg.guild.id).getPrefix() == msg.content.charAt(0)) {
+        command = msg.content.slice(1);
+
+        let args;
+        commands.get(command).run(msg,args);
+    }
+
 });
 
 client.login(token);
