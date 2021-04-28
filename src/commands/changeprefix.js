8d1ca5cd4cea8ef7fbe2dd84a0ca43dec44cf880
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-04-29 00:21:43 +0100

    sanitized prefixes

--- src/commands/changeprefix.js
+++ src/commands/changeprefix.js
@@ -1,12 +1,22 @@
 module.exports = {
     name: 'changeprefix',
     run: (msg,args) => {
-        let status = msg.guild.repository.setPrefix(args[0]);
-        if (status === false) {
-            msg.reply('There has been an error. Please try again.');
+        const prefixRegex = new RegExp(/^([^~\\'])$/);
+
+        if (args.length == 0) {
+            msg.reply(`Please supply a prefix e.g. ${msg.guild.prefix}changeprefix /`)
+        }
+        else if (!prefixRegex.test(args[0])) {
+            msg.reply('Prefix cannot be any of the following: ^ ~ \\ \'');
         }
         else {
-            msg.reply(`The prefix has been changed to \"${args[0]}\"`);
+            let status = msg.guild.repository.setPrefix(args[0]);
+            if (status === false) {
+                msg.reply('There has been an error. Please try again.');
+            }
+            else {
+                msg.reply(`The prefix has been changed to '${args[0]}.'`);
+            }
         }
     }
 }
