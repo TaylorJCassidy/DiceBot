80a064cd48ab842cecc9b29c24e3e7137ae395e0
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-04-29 00:22:12 +0100

    added help command

---
+++ src/commands/help.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'help',
+    run: (msg,args) => {
+        const Discord = require('discord.js');
+        const prefix = msg.guild.prefix;
+
+        const help = 
+         `${prefix}ping          Checks if the bot is still active\
+        \n${prefix}uptime        Shows uptime of the bot\
+        \n${prefix}maths         Evaluate a maths expression\
+        \n${prefix}d20           Rolls any dice .dice for info\
+        \n${prefix}alias add     Adds an alias .alias for info\
+        \n${prefix}alias remove  Removes an alias .alias for info\
+        \n${prefix}rig high      Rigs the dice to roll the maximum value\
+        \n${prefix}rig low       Rigs the dice to roll the minimum value\
+        \n${prefix}rig status    Shows if dice is rigged or not\
+        \n${prefix}rig toggle    Toggles whether users can rig\
+        \n${prefix}stats         Shows random dnd stats\
+        \n${prefix}getmod        Shows ability modifier of a number\
+        \n${prefix}changeprefix  Changes prefix. Currently '${prefix}'`
+        const finalHelp = new Discord.MessageEmbed().setDescription('```' + help + '```').setTitle('Dice Info');
+        msg.channel.send(finalHelp);
+    }
+}
