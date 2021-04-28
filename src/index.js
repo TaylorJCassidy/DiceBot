b3a2c01d626aa51f9d3d90dd15c5bf9c25ef9d71
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-04-28 19:19:53 +0100

    added special dice case and regex

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const {token} = require("./config/token.json");
 const client = new Discord.Client();
 const commands = require('./utils/getCommands.js').getCommands();
 let guildRepos;
+const diceRegex = new RegExp(/^((((\d{0,4}d\d{1,6})|-?\d{1,6})[\+\-\*\/])*(\d{0,4}d\d{1,6})([\+\-\*\/]\d{1,6})*)$/,'i');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -12,18 +13,26 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 
-    msg.guild.repository = guildRepos.get(msg.guild.id)
-    const prefix = msg.guild.repository.getPrefix();
+    msg.guild.repository = guildRepos.get(msg.guild.id);
+    msg.guild.prefix = msg.guild.repository.getPrefix();
 
-    if (msg.author.id !== '774637611482349578' && msg.content.startsWith(prefix) === true) {
-        const args = msg.content.slice(prefix.length).trim().split(' ');
-        const command = args.shift().toLowerCase();
+    if (msg.author.id !== '774637611482349578' && msg.content.startsWith(msg.guild.prefix)) {
+        msgcontent = msg.content.slice(msg.guild.prefix.length).trim();
 
-        if (commands.has(command)) {
-            commands.get(command).run(msg,args);
+        let dicecontent = msgcontent.replace(/ /g,'');
+        if (diceRegex.test(dicecontent)) {
+            commands.get('dice').roll(msg,dicecontent);
         }
         else {
-            msg.reply(`There is no ${command} command!`)
+            const args = msgcontent.split(' ');
+            const command = args.shift().toLowerCase();
+
+            if (commands.has(command)) {
+                commands.get(command).run(msg,args);
+            }
+            else {
+                msg.reply(`There is no ${command} command!`);
+            }
         }
     }
 
