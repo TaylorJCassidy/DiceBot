b6dc9894a2913d7b648196840366d346466da626
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-04-29 17:14:23 +0100

    added alias field and gets and sets

--- src/models/Guilds.js
+++ src/models/Guilds.js
@@ -1,14 +1,24 @@
 class Guilds {
 
     static defaults = {
-        prefix: '.'
+        prefix: '.',
+        aliases: []
     }
     prefix
+    #aliases;
 
-    constructor(prefix) {
-        this.prefix = prefix || Guilds.defaults.prefix;
+    constructor(newGuild) {
+        this.prefix = newGuild.prefix || Guilds.defaults.prefix;
+        this.#aliases = newGuild.aliases || Guilds.defaults.aliases;
     }
 
+    get aliases() {
+        return new Map(this.#aliases);
+    }
+
+    set aliases(aliases) {
+        this.#aliases = Array.from(aliases.entries());
+    }
 }
 
 module.exports = Guilds;
