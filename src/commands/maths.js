f0056b7c1ff1b5a4a5b3b6a800d3a9c206c36964
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-04-29 16:06:34 +0100

    sanitized final equation

--- src/commands/maths.js
+++ src/commands/maths.js
@@ -1,10 +1,13 @@
 module.exports = {
     name: 'maths',
+    aliases: ['m','math'],
     run: (msg,args) => {
         let eq = '';
         for (arg of args) {
             eq += arg;
         }
+        eq = eq.replace(/[^0-9%^*\/()\-+.]/g,'')
+        eq = eq.replace(/\^/g,'**')
         msg.reply(new Function('return ' + eq)())
     }
 }
