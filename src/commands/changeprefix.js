e0d2e499d8d7d1aed91b28b9bdf118eb1887528e
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-05-01 01:14:40 +0100

    changed error message

--- src/commands/changeprefix.js
+++ src/commands/changeprefix.js
@@ -1,16 +1,15 @@
 module.exports = {
     name: 'changeprefix',
     run: (msg,args) => {
-        const prefixRegex = new RegExp(/^([^~\\'])$/);
 
         if (args.length == 0) {
-            msg.reply(`Please supply a prefix e.g. ${msg.guild.prefix}changeprefix /`)
+            msg.reply(`Please supply a prefix e.g. ${msg.guild.cache.getPrefix()}changeprefix /`)
         }
-        else if (!prefixRegex.test(args[0])) {
-            msg.reply('Prefix cannot be any of the following: ^ ~ \\ \'');
+        else if (!/^([^~\\'])$/.test(args)) {
+            msg.reply('Prefix cannot be more than one character and cannot be any of the following: ^ ~ \\ \'');
         }
         else {
-            let status = msg.guild.cache.setPrefix(args[0]);
+            let status = msg.guild.cache.setPrefix(args);
             if (status === false) {
                 msg.reply('There has been an error. Please try again.');
             }
