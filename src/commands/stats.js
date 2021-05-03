1864dd9d45b465cafbad9ee88c423f0971f38da9
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-05-03 17:21:26 +0100

    added stats functionality

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,6 +1,27 @@
 module.exports = {
     name: 'stats',
     run: function(msg,args) {
-        
+        const {randomNoGen} = require('../utils/randomNoGen.js');
+        const Discord = require('discord.js');
+
+        let msgReturn = '\n>>> ';
+        const getMod = msg.client.commands.get('getmod').getMod;
+
+        for (let i=0;i<6;i++) {
+            let results = randomNoGen(6,1);
+            let smallest = results;
+
+            for (let j=0;j<3;j++) {
+                let result = randomNoGen(6,1);
+                if (result < smallest) {
+                    smallest = result;
+                }
+                results += result;
+            }
+            results -= smallest;
+            
+            msgReturn += `${results}${(results < 10 ? '  ' : ' ')}${getMod(results)}\n`
+        }
+        msg.reply(msgReturn);
     }
 }
