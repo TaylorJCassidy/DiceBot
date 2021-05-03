7dbc7b1b83b04828d182daafcc9938df604dcd07
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-05-03 21:05:06 +0100

    added error message for no provided equation

--- src/commands/maths.js
+++ src/commands/maths.js
@@ -2,13 +2,15 @@ module.exports = {
     name: 'maths',
     aliases: ['m','math'],
     run: function(msg,args) {
-        let eq = args;
-        if (/[^0-9%^*\/()\-+.]/g.test(eq)) {
+        if (args.length == 0) {
+            msg.reply(`Please supply an equation e.g. ${msg.guild.cache.getPrefix()}maths 2+2`);
+        }
+        else if (/[^0-9%^*\/()\-+.]/g.test(args)) {
             msg.reply('The equation only contain the following operators: () - + * / ^ %');
         }
         else {
-            eq = eq.replace(/\^/g,'**')
-            msg.reply(new Function('return ' + eq)())
+            args = args.replace(/\^/g,'**')
+            msg.reply(new Function('return ' + args)())
         }
     }
 }
