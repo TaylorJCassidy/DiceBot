ce4ec0543bcf2c253d335a5e7485045bd674aa0f
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-05-05 01:08:00 +0100

    user now requires admin to change prefix

--- src/commands/changeprefix.js
+++ src/commands/changeprefix.js
@@ -2,20 +2,28 @@ module.exports = {
     name: 'changeprefix',
     run: function(msg,args) {
 
-        if (args.length == 0) {
-            msg.reply(`Please supply a prefix e.g. ${msg.guild.cache.getPrefix()}changeprefix /`)
-        }
-        else if (!/^([^~\\']{1,2})$/.test(args)) {
-            msg.reply('Prefix cannot be more than two characters and cannot be any of the following: ^ ~ \\ \'');
-        }
-        else {
-            let status = msg.guild.cache.setPrefix(args);
-            if (status === false) {
-                msg.reply('There has been an error. Please try again.');
+        if (msg.member.hasPermission('ADMINISTRATOR')) {
+            if (args.length == 0) {
+                msg.reply(`Please supply a prefix e.g. ${msg.guild.cache.getPrefix()}changeprefix /`)
+            }
+            else if (!/^([^~\\']{1,2})$/.test(args)) {
+                msg.reply('Prefix cannot be more than two characters and cannot be any of the following: ^ ~ \\ \'');
+            }
+            else if (msg.guild.cache.getPrefix() == args) {
+                msg.reply(`Prefix is already '${args}'.`)
             }
             else {
-                msg.reply(`The prefix has been changed to '${msg.guild.cache.getPrefix()}'`);
+                let status = msg.guild.cache.setPrefix(args);
+                if (status === false) {
+                    msg.reply('There has been an error. Please try again.');
+                }
+                else {
+                    msg.reply(`The prefix has been changed to '${msg.guild.cache.getPrefix()}'.`);
+                }
             }
+        }
+        else {
+            msg.reply('You do not have the permission to do this action.');
         }
     }
 }
