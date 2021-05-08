45269bf35cf62d5939c1341409a3ff79adf20d81
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-05-08 21:22:46 +0100

    fixed alias not being called correctly

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ client.on('message', msg => {
                 const args = msgcontent.substring(split+1).trim();
 
                 if (aliases.has(command)) {
-                    commands.get('dice').diceController(msg,aliases.get(command)+args);
+                    commands.get('dice').diceController(msg,aliases.get(command).dice+args);
                 }
                 else if (commands.has(command)) {
                     commands.get(command).run(msg,args);
