fd3025bae62c7fb193aa7cd272c4fce68287d91e
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-05-11 01:13:35 +0100

    added source command

---
+++ src/commands/source.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'source',
+    aliases: ['sourcecode','github'],
+    run: function(msg,args) {
+        msg.reply('All of DiceBot\'s source code can be found here: https://github.com/TaylorJCassidy/DiceBot');
+    }
+}
