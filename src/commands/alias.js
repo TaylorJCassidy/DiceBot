d0271a072dada77897b87491ce869aa8d8eff66a
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-05-11 02:14:57 +0100

    added alias edit functionality

--- src/commands/alias.js
+++ src/commands/alias.js
@@ -11,29 +11,34 @@ module.exports = {
             case 'add':
                 this.addAlias(msg,args);
                 break;
+            case 'edit':
+                this.editAlias(msg,args);
+                break;
             case 'remove':
                 this.deleteAlias(msg,args);
-                break
+                break;    
             case 'list':
                 this.listAliases(msg);
-                break;
+                break;      
             case 'help':
-            default:
                 this.help(msg);
+                break;
+            default:
+                msg.reply(`There is no ${arguement} command ${msg.guild.cache.getPrefix()}alias help for help.`)
         }
     },
-    addAlias: function(msg,args) {
-        const diceRegex = new RegExp(/^((((\d{0,3}d\d{1,5})|-?\d{1,5}) ?[\+\-\*\/] ?)*(\d{0,3}d\d{1,5})( ?[\+\-\*\/] ?\d{1,5})*( ?~(res|vul|a|d))*)$/,'i');
+
+    addAlias: function(msg,args) {     
         const split = args.search(/(?<=^\w+) /);
         if (split < 1) {
-            msg.reply(`Invalid formatting ${prefix}alias add <name> <dice>`);
+            msg.reply(`Invalid formatting ${msg.guild.cache.getPrefix()}alias add <name> <dice>`);
         }
         else {
             const name = args.substring(0,split);
             const dice = args.substring(split+1);
 
-            if (!diceRegex.test(dice)) {
-                msg.reply('The dice provided is an invalid dice roll. Make sure you name has no spaces.');
+            if (!msg.client.diceRegex.test(dice)) {
+                msg.reply('The dice provided is an invalid dice roll. Please check the alias name has no spaces.');
             }
             else if (!/^(\w+)$/.test(name)){
                 msg.reply('Invalid name. The name provided includes invalid characters.');
@@ -53,7 +58,43 @@ module.exports = {
                 }
             }
         }
+        
     },
+
+    editAlias: function(msg,args) {
+        const split = args.search(/(?<=^\w+) /);
+        if (split < 1) {
+            msg.reply(`Invalid formatting ${msg.guild.cache.getPrefix()}alias edit <name> <dice>`);
+        }
+        else {
+            const name = args.substring(0,split);
+            const dice = args.substring(split+1);
+
+            if (!msg.client.diceRegex.test(dice)) {
+                msg.reply('The dice provided is an invalid dice roll. Please check the alias name has no spaces.');
+            }
+            else if (!msg.guild.cache.getAliases().has(name)) {
+                msg.reply('Invalid alias name. The alias provided does not exist.');
+            }
+            else {
+                const alias = msg.guild.cache.getAliases().get(name);
+                if (msg.author.id == alias.userID || msg.member.hasPermission('ADMINISTRATOR')) {
+                    alias.dice = dice;
+                    let status = msg.guild.cache.updateAlias(alias);
+                    if (!status) {
+                        msg.reply('There has been an error. Please try again.');
+                    }
+                    else {
+                        msg.reply(`Alias '${name}' has been edited.`);
+                    }
+                }
+                else {
+                    msg.reply('You cannot edit an alias that was not created by you.');
+                }
+            }
+        }
+    },
+
     deleteAlias: function(msg,args) {
         if (!/^(\w+)$/.test(args)){
             msg.reply('Invalid name. The name provided includes invalid characters.');
@@ -71,15 +112,16 @@ module.exports = {
                     }
                 }
                 else {
-                    msg.reply('You cannot remove an alias that was not created by you.')
+                    msg.reply('You cannot remove an alias that was not created by you.');
                 }
             }
             else {
-                msg.reply(`There is no '${args}' alias.`)
+                msg.reply(`There is no '${args}' alias.`);
             }
         }
         
-    },
+    }, 
+
     listAliases: function(msg) {
         const Discord = require('discord.js');
 
@@ -89,12 +131,13 @@ module.exports = {
         });
         msg.channel.send(new Discord.MessageEmbed().setDescription('```' + msg.guild.name + '\'s Aliases:\t\t\n\n' + msgReturn + '```').setTitle('Alias List'));
     },
+
     help: function(msg) {
         const Discord = require('discord.js');
         const prefix = msg.guild.cache.getPrefix();
         const help = `Can be used to save dice rolls for easy access later.\
         \nTo add an alias format as such:\n\
-        \n${prefix}alias add <name> <dice>\
+        \n${prefix}alias add <alias name> <dice>\
         \n${prefix}alias add attack1 3d6+10\
         \n${prefix}alias add chr d20-2\
         \n${prefix}alias add flank1 4d8+3 ~a\
@@ -103,6 +146,10 @@ module.exports = {
         \n${prefix}alias remove <alias name>\
         \n${prefix}alias remove attack1\
         \n${prefix}alias remove chr\n\
+        \nTo edit an existing alias, format as such:\n\
+        \n${prefix}alias edit <alias name> <new dice>\
+        \n${prefix}alias edit attack1 2d6+2\
+        \n${prefix}alias edit chr d20+3 ~a\n\
         \nTo use a alias, format as such:\n\
         \n${prefix}attack1\
         \n${prefix}chr\n\
