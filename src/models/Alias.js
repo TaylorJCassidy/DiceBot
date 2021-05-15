74f6737f90c0c8fc01aa4140deec7aa3b46b72e0
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-05-15 02:06:22 +0100

    added documentation

--- src/models/Alias.js
+++ src/models/Alias.js
@@ -1,10 +1,25 @@
+/**
+ * @class
+ * Dicebots Alias model class. This should be used whenever a alias is read or needs to be written to a filestore.
+ */
 class Alias {
 
+    /**@private*/
     _guildID;
+    /**@private*/
     _userID;
+    /**@private*/
     _aliasName;
+    /**@private*/
     _dice;
-    
+
+    /**
+     * Default constructor
+     * @param {String} guildID Discord guild ID of guild where alias was created
+     * @param {String} userID Discord user ID of alias author
+     * @param {String} aliasName alias name
+     * @param {String} dice dice to roll
+     */
     constructor(guildID,userID,aliasName,dice) {
         this._guildID = guildID;
         this._userID = userID;
@@ -12,34 +27,75 @@ class Alias {
         this._dice = dice;
     }
 
+    /**
+     * Gets alias' guild ID
+     * @returns {String} alias' guild ID
+     */
     get guildID() {
         return this._guildID;
     }
-    set guildID(value) {
-        this._guildID = value;
+
+    /**
+     * Sets the alias' guild ID
+     * @param {String} guildID alias' guild ID to set
+     */
+    set guildID(guildID) {
+        this._guildID = guildID;
     }
 
+    /**
+     * Gets the alias' userID
+     * @returns {String} alias' user ID
+     */
     get userID() {
         return this._userID;
     }
-    set userID(value) {
-        this._userID = value;
+
+    /**
+     * Sets the alias' userID
+     * @param {String} userID alias' user ID to set
+     */
+    set userID(userID) {
+        this._userID = userID;
     }
 
+    /**
+     * Gets the alias name
+     * @returns {String} the alias name
+     */
     get aliasName() {
         return this._aliasName;
     }
-    set aliasName(value) {
-        this._aliasName = value;
+
+    /**
+     * Sets the alias name
+     * @param {String} aliasName the alias name to set
+     */
+    set aliasName(aliasName) {
+        this._aliasName = aliasName;
     }
 
+    /**
+     * Gets the alias' dice
+     * @returns {String} the alias' dice
+     */
     get dice() {
         return this._dice;
     }
-    set dice(value) {
-        this._dice = value;
+
+    /**
+     * Sets the alias' dice
+     * @param {String} dice the alias' dice to set
+     */
+    set dice(dice) {
+        this._dice = dice;
     }
 
+    /**
+     * Returns Alias object as a plain javascript object without methods
+     * This is used by JSON.stringify and should not be modified
+     * @returns {Object} Alias object
+     */
     toJSON() {
         return {
             guildID: this._guildID,
--- src/models/Guild.js
+++ src/models/Guild.js
@@ -1,62 +1,129 @@
+const Alias = require("./Alias");
+
+/**
+ * @class
+ * Dicebots Guild model class. This should be used whenever a guild is read or needs to be written to a filestore.
+ */
 class Guild {
 
+    /**@private */
     _guildID;
+    /**@private */
     _prefix;
-    //-1- unriggable 0-not rigged 1-rigged low 2-rigged high
+    /**
+     * @private
+     * -1- unriggable 0-not rigged 1-rigged low 2-rigged high
+     */
     _rigged;
+    /**@private */
     _aliases;
-
+    
+    /**
+     * Default constructor
+     * @param {String} guildID Discord guild ID of guild
+     * @param {String} prefix Guilds prefix
+     * @param {Array} aliases Any aliases to be attached to the guild
+     * @param {Number} rigged Rigged number -1-unriggable 0-not rigged 1-rigged low 2-rigged high
+     */
     constructor(guildID,prefix,aliases,rigged) {
         this._guildID = guildID;
         this._prefix = prefix || '.'; 
-        this._rigged = rigged || 0;
-        this._aliases = new Map(aliases);
-        
+        this._rigged = Guild._validRigged(rigged) || 0;
+        this._aliases = new Map(aliases);     
     }
 
+    /**
+     * Gets the guilds ID
+     * @returns {String} the guild ID
+     */
     get guildID() {
         return this._guildID;
     }
-    set guildID(value) {
-        this._guildID = value;
+
+    /**
+     * Sets the guilds ID
+     * @param {String} guildID the guild ID to set
+     */
+    set guildID(guildID) {
+        this._guildID = guildID;
     }
 
+    /**
+     * Gets the guilds prefix
+     * @returns {String} the guilds prefix
+     */
     get prefix() {
         return this._prefix;
     }
-    set prefix(value) {
-        this._prefix = value;
+
+    /**
+     * Sets the guilds prefix
+     * @param {String} prefix the prefix to set
+     */
+    set prefix(prefix) {
+        this._prefix = prefix;
     }
 
+    /**
+     * Gets the guilds aliases
+     * @returns {Map<String,Alias>} the guilds aliases
+     */
     get aliases() {
-        return this._aliases
-    }
-    set aliases(value) {
-        this._aliases = value;
+        return this._aliases;
     }
 
+    /**
+     * Sets the guilds aliases
+     * @param {Map<String,Alias>} aliases the guilds aliases to set
+     */
+    set aliases(aliases) {
+        this._aliases = aliases;
+    }
+
+    /**
+     * Gets the guilds rigged number
+     * -1-unriggable 0-not rigged 1-rigged low 2-rigged high
+     * @returns {Number} the guilds rigged number
+     */
     get rigged() {
         return this._rigged;
     }
+
     /**
-     * Set the guild's rigged number.
+     * Set the guilds rigged number
      * -1-unriggable 0-not rigged 1-rigged low 2-rigged high
-     * @param {int} rigged the rigged number
+     * @param {Number} rigged the guilds rigged number to set
      */
     set rigged(rigged) {
+        this._rigged = Guild._validRigged(rigged);
+    }
+
+    /**
+     * @private
+     * If rigged number is out of range, throws an exception
+     * @param {Number} rigged Rigged number
+     */
+    static _validRigged(rigged) {
         if (rigged < -1 || rigged > 2) {
             throw new Error('Guild rigged number must must be either >= -1 and =< 2'); 
         }
-        this._rigged = rigged;
+        else {
+            return rigged;
+        }
     }
 
+    /**
+     * Returns Guild object as a plain javascript object without methods
+     * This is used by JSON.stringify and should not be modified
+     * @returns {Object} Guild object
+     */
     toJSON() {
         return {
             guildID: this.guildID,
             prefix: this.prefix,
             rigged: this.rigged,
             aliases: Array.from(this.aliases)
-        }
+        };
     }
 }
 
