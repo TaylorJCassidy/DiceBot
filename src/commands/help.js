a9d1926813b4b28775cc28e030bed8210a7b4e40
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-05-18 20:41:48 +0100

    help message is now in alphabetical order

--- src/commands/help.js
+++ src/commands/help.js
@@ -3,21 +3,21 @@ module.exports = {
     run: function(msg,args) {
         const prefix = msg.guild.cache.getPrefix();
         const help = 
-         `${prefix}ping          Gets a pong response from the bot\
-        \n${prefix}uptime        Shows uptime of the bot\
-        \n${prefix}maths         Evaluate a maths expression\
-        \n${prefix}d20           Rolls any dice ${prefix}dice for info\
-        \n${prefix}alias add     Adds an alias ${prefix}alias for info\
+         `${prefix}alias add     Adds an alias ${prefix}alias for info\
         \n${prefix}alias edit    Edits an alias ${prefix}alias for info\
-        \n${prefix}alias remove  Removes an alias ${prefix}alias for info\
         \n${prefix}alias list    Lists all aliases in this server\
+        \n${prefix}alias remove  Removes an alias ${prefix}alias for info\
+        \n${prefix}changeprefix  Changes prefix. Currently '${prefix}
+        \n${prefix}d20           Rolls any dice ${prefix}dice for info\
+        \n${prefix}getmod        Shows ability modifier of a number\
+        \n${prefix}maths         Evaluate a maths expression\  
+        \n${prefix}ping          Gets a pong response from the bot\      
         \n${prefix}rig high      Rigs the dice to roll the maximum value\
         \n${prefix}rig low       Rigs the dice to roll the minimum value\
         \n${prefix}rig status    Shows if dice is rigged or not\
         \n${prefix}rig toggle    Toggles whether users can rig .rig for info\
         \n${prefix}stats         Shows random dnd stats\
-        \n${prefix}getmod        Shows ability modifier of a number\
-        \n${prefix}changeprefix  Changes prefix. Currently '${prefix}'`
+        \n${prefix}uptime        Shows uptime of the bot\'`
         const {helpEmbed} = require('../utils/helpEmbed.js')
         msg.channel.send(helpEmbed(help,'Help'));
     }
