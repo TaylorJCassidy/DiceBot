2dcf0a3a5b83fbb95e26bee91488fcaccbc2f148
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-05-25 16:04:39 +0100

    source now includes icon source

--- src/commands/source.js
+++ src/commands/source.js
@@ -2,6 +2,7 @@ module.exports = {
     name: 'source',
     aliases: ['sourcecode','github'],
     run: function(msg,args) {
-        msg.reply('All of DiceBot\'s source code can be found here: https://github.com/TaylorJCassidy/DiceBot');
+        const {colour,iconProvider} = require('../config/theme.json');
+        msg.reply('All of DiceBot\'s source code can be found here: https://github.com/TaylorJCassidy/DiceBot. Icon is provided by ' + iconProvider);
     }
 }
