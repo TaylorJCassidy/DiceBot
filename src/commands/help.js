37d58f0d30af7e84c431454a10f80a6f77c14006
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-05-25 16:04:19 +0100

    added source command to help list

--- src/commands/help.js
+++ src/commands/help.js
@@ -7,16 +7,17 @@ module.exports = {
         \n${prefix}alias edit    Edits an alias ${prefix}alias for info\
         \n${prefix}alias list    Lists all aliases in this server\
         \n${prefix}alias remove  Removes an alias ${prefix}alias for info\
-        \n${prefix}changeprefix  Changes prefix. Currently '${prefix}
+        \n${prefix}changeprefix  Changes prefix. Currently '${prefix}\
         \n${prefix}d20           Rolls any dice ${prefix}dice for info\
         \n${prefix}getmod        Shows ability modifier of a number\
-        \n${prefix}maths         Evaluate a maths expression\  
-        \n${prefix}ping          Gets the bots response times\      
+        \n${prefix}maths         Evaluate a maths expression\
+        \n${prefix}ping          Gets the bots response times\
         \n${prefix}rig high      Rigs the dice to roll the maximum value\
         \n${prefix}rig low       Rigs the dice to roll the minimum value\
         \n${prefix}rig status    Shows if dice is rigged or not\
         \n${prefix}rig toggle    Toggles whether users can rig .rig for info\
         \n${prefix}stats         Shows random dnd stats\
+        \n${prefix}source        Shows all sources of the bot\
         \n${prefix}uptime        Shows the bots uptime\'`
         const {helpEmbed} = require('../utils/helpEmbed.js')
         msg.channel.send(helpEmbed(help,'Help'));
