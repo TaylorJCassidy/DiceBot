a98a3de34da08aed9c8d5ecc1a292e58e9b80078
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-05-26 16:04:43 +0100

    moved import outside of run method

--- src/commands/source.js
+++ src/commands/source.js
@@ -1,8 +1,9 @@
+const {colour,iconProvider} = require('../config/theme.json');
+
 module.exports = {
     name: 'source',
     aliases: ['sourcecode','github'],
     run: function(msg,args) {
-        const {colour,iconProvider} = require('../config/theme.json');
         msg.reply('All of DiceBot\'s source code can be found here: https://github.com/TaylorJCassidy/DiceBot. Icon is provided by ' + iconProvider);
     }
 }
