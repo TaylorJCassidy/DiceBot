7e02c14aa82d4649daa496d4d75457c50bb7c2cb
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-06-07 01:46:16 +0100

    fixed typo, moved import

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,3 +1,5 @@
+const {helpEmbed} = require('../utils/helpEmbed.js')
+
 module.exports = {
     name: 'help',
     run: function(msg,args) {
@@ -7,7 +9,7 @@ module.exports = {
         \n${prefix}alias edit    Edits an alias ${prefix}alias for info\
         \n${prefix}alias list    Lists all aliases in this server\
         \n${prefix}alias remove  Removes an alias ${prefix}alias for info\
-        \n${prefix}changeprefix  Changes prefix. Currently '${prefix}\
+        \n${prefix}changeprefix  Changes prefix. Currently '${prefix}'\
         \n${prefix}d20           Rolls any dice ${prefix}dice for info\
         \n${prefix}getmod        Shows ability modifier of a number\
         \n${prefix}maths         Evaluate a maths expression\
@@ -18,8 +20,7 @@ module.exports = {
         \n${prefix}rig toggle    Toggles whether users can rig .rig for info\
         \n${prefix}stats         Shows random dnd stats\
         \n${prefix}source        Shows all sources of the bot\
-        \n${prefix}uptime        Shows the bots uptime\'`
-        const {helpEmbed} = require('../utils/helpEmbed.js')
+        \n${prefix}uptime        Shows the bots uptime\'`       
         msg.channel.send(helpEmbed(help,'Help'));
     }
 }
