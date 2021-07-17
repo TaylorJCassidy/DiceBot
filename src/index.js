9d29de211a596dfa5df603fa62e174207377ff81
TaylorJCassidy <taylorjcassidy1@gmail.com>
2021-07-18 00:03:46 +0100

    added status message

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ client.once('ready', () => {
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity(`for dice in ${guildCaches.size} servers`, { type: 'WATCHING' })
 });
 
 client.on('guildDelete', (guild) => {
