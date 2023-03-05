ea149a56693b61d62c13c7b705b5e97d7916311a
TaylorJCassidy <taylorjcassidy1@gmail.com>
2023-03-05 19:06:02 +0000

    improve maths sanitation and error handling, add maths.test.js

--- src/main/commands/maths.js
+++ src/main/commands/maths.js
@@ -1,3 +1,5 @@
+const cleanRegex = /[^0-9%^*/()\-+.]/g;
+
 module.exports = {
     name: 'maths',
     aliases: ['m','math'],
@@ -5,13 +7,26 @@ module.exports = {
         if (args.length == 0) {
             msg.reply(`Please supply an equation e.g. ${msg.guild.cache.getPrefix()}maths 2+2`);
         }
-        // eslint-disable-next-line no-useless-escape
-        else if (/[^0-9%^*\/()\-+.]/g.test(args)) {
+        else if (cleanRegex.test(args)) {
             msg.reply('The equation can only contain the following operators: () - + * / ^ %');
         }
         else {
             args = args.replace(/\^/g,'**');
-            msg.reply(new Function('return ' + args)().toString());
+            msg.reply(this.calc(args));
         }
+    },
+    calc: function(equation) {
+        let toReturn;
+        if (cleanRegex.test(equation)) return 'Invalid';
+        
+        try {
+            toReturn = new Function('return ' + equation)().toString();
+        }
+        catch(e) {
+            return 'Invalid';
+        }
+        
+        if (/[^0-9.]/g.test(toReturn)) return 'Invalid';
+        return toReturn;
     }
 };
